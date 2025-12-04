c1610fd27c25c0b5cd4131bc1877b5a6080a07bf
manju071104-ship-it <manju0711@gmail.com>
2025-12-04 16:03:56 +0530

    slider

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
-document.addEventListener('DOMContentLoaded',()=>{
+document.addEventListener('DOMContentLoaded', () => {
     const menuicon = document.querySelector('.menuicon');
     const navmenu = document.querySelector('.nav-menu');
-    menuicon.addEventListener('click',()=>{
+    menuicon.addEventListener('click', () => {
         navmenu.classList.toggle('active');
     })
+
+    // creating slider 
+
+    let slideindex = 0;
+
+    function slider(index) {
+        const images = document.querySelectorAll('.images img');
+        console.log(images);
+
+        if(index >= images.length){
+            slideindex = 0;
+        }
+        images.forEach(img => {
+            img.style.display = 'none';
+        });
+        images[slideindex].style.display = 'block';
+    }
+    function next(){
+        slideindex++
+        slider(slideindex);
+    }
+    setInterval(next,3000)
+    slider(0);
 });
